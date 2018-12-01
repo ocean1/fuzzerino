b4e1466dce82f6a507db8545d20a3f26664ee1c5
ocean1 <ocean.kuzuri@gmail.com>
2018-12-01 20:19:01 +0100

    yolo

--- tests/geptest/test.c
+++ tests/geptest/test.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <stdbool.h>
 
 int main(){
 	uint8_t lol[] = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA\x00";
 
+	bool l;
 	uint32_t a = ((uint32_t*)lol)[0];
 
 	printf("a = %x", a);
-	return a;
+	l = (bool)a;
+	return l;
 }
