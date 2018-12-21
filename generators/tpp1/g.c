357e02456e8609ea42b56a8788cb9c775cbefb2b
ocean1 <ocean.kuzuri@gmail.com>
2018-12-21 15:09:05 +0100

    tpp1

---
+++ generators/tpp1/g.c
@@ -0,0 +1,51 @@
+/* basic example of generator 1
+ * more complex example will build on this
+ * to "simulate" a basic binary file */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+#include <gfuzz.h>
+
+const char* magic = "FFT"; // fast fuzzer test
+const int magic_len = 4;
+
+// default content if string hasn't been passed via cmdline
+char* dcontent = "THIS IS A G-FUZZING TEST BEWARE";
+
+
+void writefile(uint8_t *buf, uint32_t size){
+	FILE* fout;
+	fout = fopen("/dev/shm/fuzztest", "w"); // output file
+
+	fwrite(buf, size, 1, fout);
+}
+
+__fuzz void createfmt(char *content){
+	uint32_t len = strlen(content);
+	uint32_t fsize = 2 * sizeof(uint32_t) + len;
+	uint8_t *obuf, *buf;
+
+	obuf = buf = malloc(fsize);
+	
+	memcpy(obuf, magic, sizeof(uint32_t));
+	obuf += sizeof(uint32_t);
+	memcpy(obuf, &len, sizeof(uint32_t));
+	obuf += sizeof(uint32_t);
+	memcpy(obuf, content, len);
+
+	writefile(buf, fsize);
+}
+
+int main(int argc, char **argv)
+{
+	char *content;
+
+	if (argc > 1){
+		content = argv[1];
+	} else {
+		content = dcontent;
+	}
+	createfmt(content);
+}
---
+++ generators/tpp1/p.c
@@ -0,0 +1,64 @@
+/* simple parser (1) responds to g.c format */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+const char* magic = "FFT"; // fast fuzzer test
+
+
+bool readstatic(FILE* fd, uint8_t *content, uint32_t *len){
+	void* buf = content;
+	
+	fread(buf, sizeof(uint32_t), 1, fd);
+	if (*(uint32_t*)buf != *(uint32_t*)magic) {
+		fprintf(stderr, "%x %x", *(uint32_t*)magic, *(uint32_t*)buf);
+		return false;
+	}
+	buf += sizeof(uint32_t);
+
+	fread(len, sizeof(uint32_t), 1, fd);
+
+	buf += sizeof(uint32_t);
+
+	fread(buf, *len, 1, fd);
+	fprintf(stderr, "%s ", (char *)buf);
+	
+	return true;
+}
+
+unsigned char g_content[512];
+
+int main(int argc, char **argv)
+{
+	FILE* fin;
+	uint8_t l_content[512];
+	bool res = false;
+	uint32_t len;
+	uint8_t test = 0;
+
+	if (argc > 2) {
+		test = (uint8_t)atoi(argv[2]);
+	}
+
+	if (argc > 1) {
+		fin = fopen(argv[1], "r"); // output file
+	} else {
+		fin = fopen("/dev/shm/fuzztest", "r");
+	}
+	
+	switch (test) {
+		case 0:
+			res = readstatic(fin, l_content, &len);
+			break;
+		case 1:
+			res = readstatic(fin, g_content, &len);
+			break;
+		case 2:
+		default: break;
+	}
+
+	printf("result %s", res ? "true": "false");
+}
