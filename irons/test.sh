897db1a2cd1b43c4994bdd9b6085fa855cbaad7c
Michele Ferri <tiduspira@gmail.com>
2019-07-11 00:29:11 +0200

    small fix

--- irons/test.sh
+++ irons/test.sh
@@ -7,7 +7,7 @@ if [ $# -eq 1 ] && [ $1 == "-t1" ]
   then
     for i in ./out/generated/*; do
         echo === $i ===
-        ../../tests/t1/ptest $i
+        ../tests/t1/ptest $i
         echo
     done
 fi
@@ -16,7 +16,7 @@ if [ $# -eq 1 ] && [ $1 == "-t2" ]
   then
     for i in ./out/generated/*; do
         echo === $i ===
-        ../../tests/t2/ptest $i
+        ../tests/t2/ptest $i
         echo
     done
 fi
