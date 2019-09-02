5b6ff6657e2b0e897d70c16707a53c196bfe0032
Michele Ferri <tiduspira@gmail.com>
2019-09-02 16:57:51 +0200

    datasets

---
+++ eval/print_stats.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+for dir in datasets/*; do
+    echo
+    echo "=== $dir"
+    echo "    total files:" `ls $dir | wc -l`
+    echo "    avg size:" `ls $dir -l | gawk '{sum += $5; n++;} END {print sum/n;}'` "bytes"
+    echo "    largest:" `find $dir -type f | xargs ls -lS 2>/dev/null | head -n 1 | awk '{print $5}'` "bytes"
+    echo "    smallest:" `find $dir -type f | xargs ls -lrS 2>/dev/null | head -n 1 | awk '{print $5}'` "bytes"
+done
